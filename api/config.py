c9f756ca9d73a6d97cce1f29924b1cee33277fa2
ahmed-10 <ahmedalaa.10@hotmail.com>
2022-07-22 02:58:48 +0200

    chore: add database uri to app config

--- api/config.py
+++ api/config.py
@@ -8,16 +8,19 @@ class Config(object):
     DEBUG = False
     TESTING = False
     SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProductionConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.getenv('PROD_DATABASE_URL')
 
 
 class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.getenv('DEV_DATABASE_URL')
     DEBUG = True
 
 
 class TestingConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URL')
     TESTING = True
     DEBUG = False
