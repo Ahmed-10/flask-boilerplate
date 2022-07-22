1968ad7d0abf54f4392d822dab48adbf44e726fa
ahmed-10 <ahmedalaa.10@hotmail.com>
2022-07-22 03:47:29 +0200

    test: database test config

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,17 @@
 import pytest
 from api import create_app
-
+from api.models import db
 
 @pytest.fixture(scope="session")
 def app():
     app = create_app('api.config.TestingConfig')
 
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+
     yield app
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def client(app):
     return app.test_client()
